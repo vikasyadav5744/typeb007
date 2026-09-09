b9f2cae1be4bdf39fc5750be15a83720e234fc45
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-09-09 13:06:16 +0530

    Update codetypeb.py

--- codetypeb.py
+++ codetypeb.py
@@ -367,7 +367,8 @@ epoch_len =len(list_epoch)
 with st.expander (" serial no.7 is 1st-Sep-2026 expiry"):
   st.write(list_epoch)
 
-dictn = expiry['data']['dctExp'].keys().to_numeric()
+dictn = expiry['data']['dctExp'].keys()
+dictn = expiry['data']['dctExp'].keys()
 st.write(dictn)
  
 master_chain = pd.DataFrame(expiry)
