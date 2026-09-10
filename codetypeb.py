8de50e7ae104e3123ac5e60e61c7d91f02d2b6ba
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-09-10 15:03:10 +0530

    Update codetypeb.py

--- codetypeb.py
+++ codetypeb.py
@@ -273,7 +273,7 @@ contract = result101["data"]["contractModel"]
 st.write(expiry101)
 st.write(contract)
 
-st.write(list_epoch)
+st.write(list_epoch[0])
 month_exp=[]
 for i in list_epoch:
   exp = requests.get(f"{url}/openapi/typea/GetOptionChain/2/{list_epoch[i]}/26000", headers=headers3)
