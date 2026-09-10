da3b3d9dbed87113736c78b66cf88c555c932aa2
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-09-10 16:06:17 +0530

    Update codetypeb.py

--- codetypeb.py
+++ codetypeb.py
@@ -292,8 +292,8 @@ spot2 = json.loads(spot1)
 Time = spot2['data']['candles'][0][0]
 Nifty_Close = round(spot2['data']['candles'][0][3])
 Nifty_round = round(spot2['data']['candles'][0][3], -2)
-Nifty_round_lower = round(spot2['data']['candles'][0][3], -2)- 800
-Nifty_round_upper = round(spot2['data']['candles'][0][3], -2) + 800
+Nifty_round_lower = round(spot2['data']['candles'][0][3], -2)- 400
+Nifty_round_upper = round(spot2['data']['candles'][0][3], -2) + 400
 
 st.write("Spot Nifty Intraday", Time, Nifty_Close, Nifty_round)
 
