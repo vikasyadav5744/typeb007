641f2b40929d476b2ba28f0b16198c522eca1270
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-09-10 15:46:20 +0530

    Update codetypeb.py

--- codetypeb.py
+++ codetypeb.py
@@ -280,7 +280,7 @@ for i in list_epoch:
   exp = requests.get(f"{url}/openapi/typea/GetOptionChain/2/{i}/26000", headers=headers3)
   if exp.status_code==200:  
     result101 = exp.json()
-    exp1 = result101["data"]
+    exp1 = result101["data"]["contractModel"]
     month_exp.append(exp1)
     st.write(result101)
   else:
